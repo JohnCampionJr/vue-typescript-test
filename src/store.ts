a38bfb88441e0646565d5a90cabc7908b5a11222
John Campion Jr <john@brightshore.com>
2018-10-26 21:38:56 -0400

    Initial attempt at vuex-module-decorators

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,33 @@
+import Vuex, { Module as Mod } from 'vuex';
 import Vue from 'vue';
-import Vuex from 'vuex';
-
 Vue.use(Vuex);
+import { Action, Module, Mutation, MutationAction, VuexModule, getModule } from 'vuex-module-decorators';
 
-export default new Vuex.Store({
-  state: {
+interface StoreType {
+    mm: MyModule;
+}
 
-  },
-  mutations: {
+@Module({ name: 'mm' })
+class MyModule extends VuexModule {
+    public count = 0;
 
-  },
-  actions: {
+    @Mutation
+    public incrCount(delta: number) {
+        this.count += delta;
+    }
 
-  },
+    @Action({ commit: 'incrCount' })
+    public async getCountDelta(retVal: number = 5) {
+        return retVal;
+    }
+
+    public get halfCount() {
+        return (this.count / 2).toPrecision(1);
+    }
+}
+
+export default new Vuex.Store<StoreType>({
+    modules: {
+        mm: MyModule,
+    },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,26 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{ test }}
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { getModule } from 'vuex-module-decorators';
+// import { CounterModule } from '@/modules/CounterModule.ts';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public test: string = 'tester';
+  // public counterModule = getModule(CounterModule);
+  public mounted() {
+    // CounterModule.increment(5);
+  }
+}
 </script>
