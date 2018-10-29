8dface445b953f6a552908b5110f9c16fa79fb9e
John Campion Jr <john@brightshore.com>
2018-10-29 17:47:43 -0400

    Added vue-cli-plugin-auto-routing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
     {{ test }}
-    {{ myModule.count }}
+    {{ myModule.count }}    
   </div>
 </template>
 
